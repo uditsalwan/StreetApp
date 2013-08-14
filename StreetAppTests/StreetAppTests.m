0fb165eb219211b7a0e9d401546780f57e0307d4
nagarro <nagarro@nagarros-MacBook-Pro-7.local>
2013-08-14 12:28:20 +0530

    Initial Commit

---
+++ StreetAppTests/StreetAppTests.m
@@ -0,0 +1,36 @@
+//
+//  StreetAppTests.m
+//  StreetAppTests
+//
+//  Created by nagarro on 8/14/13.
+//  Copyright (c) 2013 self. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StreetAppTests : XCTestCase
+
+@end
+
+@implementation StreetAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
